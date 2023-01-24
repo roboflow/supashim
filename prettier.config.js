4f324795af37d90d5558c56a1f20177517c6790a
Brad Dwyer <brad@roboflow.ai>
2023-01-24 13:49:41 -0600

    Repo setup

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    printWidth: 100,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: false,
+    quoteProps: "consistent",
+    jsxSingleQuote: false,
+    trailingComma: "none",
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: "always",
+    proseWrap: "preserve",
+    htmlWhitespaceSensitivity: "strict",
+    endOfLine: "lf"
+};
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist/umd"),
+        filename: "supashim.js",
+        library: {
+            type: "umd",
+            name: "supashim"
+        }
+    },
+    module: {
+        rules: [
+            // {
+            //     test: /\.ts$/,
+            //     loader: "ts-loader",
+            //     options: {
+            //         transpileOnly: true
+            //     }
+            // }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js", ".json"]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            process: "process/browser"
+        })
+    ]
+};
